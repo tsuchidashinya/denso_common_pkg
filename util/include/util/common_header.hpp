309aeaeab1f8465e1610d8a981ba36dd916b00c8
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2022-10-15 14:33:37 +0900

    途中

--- util/include/util/common_header.hpp
+++ util/include/util/common_header.hpp
@@ -13,6 +13,8 @@
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/static_transform_broadcaster.h>
 #include <tf/transform_datatypes.h>
 #include <random>
 #include <iomanip>
