570ff14e8a642f4935656525d727c5ed3c177201
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2022-11-24 11:56:59 +0000

    fix: hdf5_record_server

--- hdf5_package/scripts/hdf5_record_server.py
+++ hdf5_package/scripts/hdf5_record_server.py
@@ -5,6 +5,7 @@ from anno_srvs.srv import RecordAcc, RecordAccResponse, RecordAccRequest
 from anno_srvs.srv import RecordClustering, RecordClusteringResponse
 from anno_srvs.srv import RecordSegmentation, RecordSegmentationResponse
 from anno_srvs.srv import RecordPoseEstimation, RecordPoseEstimationResponse
+from anno_srvs.srv import RecordRealSensorData, RecordRealSensorDataResponse
 import rospy
 import rosparam
 from util import util
@@ -22,6 +23,7 @@ class RecordServiceClass():
         rospy.Service(self.record_segmentation_name, RecordSegmentation, self.record_segmentation_service_callback)
         rospy.Service(self.record_pose_estimation_name, RecordPoseEstimation, self.record_pose_estimation_service_callback)
         rospy.Service(self.record_clustering_name, RecordClustering, self.record_clustering_service_callback)
+        rospy.Service(self.record_real_sensor_service_name, RecordRealSensorData, self.record_real_sensor_data_service_callback)
 
     def set_parameter(self):
         param_list = rosparam.get_param(rospy.get_name() + "/hdf5_record_server/")
@@ -33,6 +35,7 @@ class RecordServiceClass():
         self.hdf5_file_name = param_list["record_hdf5_file_name"]
         self.hdf5_save_interval = param_list["hdf5_save_interval"]
         self.hdf5_service_counter = 0
+        self.record_real_sensor_service_name = param_list["record_real_sensor_data_service_name"]
         
     def record_acc_service_callback(self, request):
         # request = RecordAccRequest()
@@ -139,6 +142,33 @@ class RecordServiceClass():
         self.hdf5_service_counter = self.hdf5_service_counter + 1
         response = RecordClusteringResponse()
         response.ok = True
+        return response
+    
+    def record_real_sensor_data_service_callback(self, request):
+        response = RecordRealSensorDataResponse()
+        response.ok = True
+        self.hdf5_service_counter = self.hdf5_service_counter + 1
+        index = self.hdf5_service_counter % self.hdf5_save_interval
+        if self.hdf5_service_counter == 1:
+            self.bar = tqdm(total=request.the_number_of_dataset)
+            self.bar.set_description("Progress rate")
+            self.hdf5_file_dir = util.dir_join_and_make(self.hdf5_file_dir, util.exclude_ext_str(self.hdf5_file_name) + util.get_time_str_dir())
+            self.hdf5_object = hdf5_function.open_writed_hdf5(util.decide_allpath(self.hdf5_file_dir, self.hdf5_file_name))
+        elif request.the_number_of_dataset == self.hdf5_service_counter:
+            self.bar.update(1)
+            hdf5_function.close_hdf5(self.hdf5_object)
+            hdf5_function.concatenate_hdf5(self.hdf5_file_dir, util.decide_allpath(self.hdf5_file_dir, self.hdf5_file_name))
+            return response
+        elif index == 0:
+            hdf5_function.close_hdf5(self.hdf5_object)
+            self.hdf5_object = hdf5_function.open_writed_hdf5(util.decide_allpath(self.hdf5_file_dir, self.hdf5_file_name))
+        np_cloud = util_msg_data.msgcloud_to_npcloud(request.cloud_data)
+        np_cloud, _ = util_msg_data.extract_mask_from_npcloud(np_cloud)
+        np_cam = util_msg_data.msgcam_to_npcam(request.camera_info)
+        np_img = util_msg_data.rosimg_to_npimg(request.image)
+        data_dict = {"Points": np_cloud, "image": np_img, "camera_info": np_cam}
+        hdf5_function.write_hdf5(self.hdf5_object, data_dict, index)
+        self.bar.update(1)
         return response
     
         
