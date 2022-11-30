6328635b2bc09abca4c6ea64d25f896ee3b80563
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2022-11-30 15:18:07 +0000

    add: tf_delete_server

--- tf_package/include/tf_package/tf_broadcast_server.hpp
+++ tf_package/include/tf_package/tf_broadcast_server.hpp
@@ -3,6 +3,7 @@
 #include <util/util.hpp>
 #include <tf_package/tf_function.hpp>
 #include <common_srvs/TfBroadcastService.h>
+#include <common_srvs/TfDeleteService.h>
 
 class TfBroadcastServer
 {
@@ -10,15 +11,16 @@ public:
     TfBroadcastServer(ros::NodeHandle &);
     void timer_callback(const ros::TimerEvent&);
     bool tf_broadcast_service_callback(common_srvs::TfBroadcastService::Request&, common_srvs::TfBroadcastService::Response&);
+    bool tf_delete_service_callback(common_srvs::TfDeleteService::Request&, common_srvs::TfDeleteService::Response&);
     void set_parameter();
 private:
     ros::NodeHandle nh_, pnh_;
-    std::string tf_broadcast_service_name_;
+    std::string tf_broadcast_service_name_, tf_delete_service_name_;
     double timer_duration_;
     TfFunction tf_function_;
     std::vector<std::string> tf_name_list_;
     std::vector<geometry_msgs::TransformStamped> tf_stamp_list_;
     XmlRpc::XmlRpcValue param_list;
     ros::Timer timer_;
-    ros::ServiceServer server_;
+    ros::ServiceServer server_, delete_server_;
 };
--- tf_package/src/tf_broadcast_server.cpp
+++ tf_package/src/tf_broadcast_server.cpp
@@ -7,6 +7,7 @@ pnh_("~")
     set_parameter();
     server_ = nh_.advertiseService(tf_broadcast_service_name_, &TfBroadcastServer::tf_broadcast_service_callback, this);
     timer_ = nh_.createTimer(ros::Duration(timer_duration_), &TfBroadcastServer::timer_callback, this);
+    delete_server_ = nh_.advertiseService(tf_delete_service_name_, &TfBroadcastServer::tf_delete_service_callback, this);
 }
 
 void TfBroadcastServer::set_parameter()
@@ -14,6 +15,7 @@ void TfBroadcastServer::set_parameter()
     pnh_.getParam("tf_broadcast_server", param_list);
     timer_duration_ = param_list["timer_duration"];
     tf_broadcast_service_name_ = static_cast<std::string>(param_list["tf_broadcast_service_name"]);
+    tf_delete_service_name_ = static_cast<std::string>(param_list["tf_delete_service_name"]);
 }
 
 bool TfBroadcastServer::tf_broadcast_service_callback(common_srvs::TfBroadcastService::Request &request, 
@@ -29,6 +31,23 @@ bool TfBroadcastServer::tf_broadcast_service_callback(common_srvs::TfBroadcastSe
         tf_stamp_list_[index] = request.broadcast_tf;
     }
     response.ok = true;
+    return true;
+}
+
+bool TfBroadcastServer::tf_delete_service_callback(common_srvs::TfDeleteService::Request &request,
+                                            common_srvs::TfDeleteService::Response &response)
+{
+    int index = Util::find_element_vector(tf_name_list_, request.delete_tf_name);
+    if (index == -1) {
+        response.is_delete_completed = 0;
+    }
+    else {
+        tf_stamp_list_[index].header.frame_id = "";
+        tf_function_.broadcast(tf_stamp_list_[index]);
+        tf_name_list_.erase(tf_name_list_.begin() + index);
+        tf_stamp_list_.erase(tf_stamp_list_.begin() + index);
+        response.is_delete_completed = 1;
+    }
     return true;
 }
 
