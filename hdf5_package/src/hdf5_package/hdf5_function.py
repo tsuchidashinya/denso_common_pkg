b32ea0b9754811eeac648c16c5368ac2b7627393
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2022-12-22 20:51:12 +0000

    fix: hdf5_function

--- hdf5_package/src/hdf5_package/hdf5_function.py
+++ hdf5_package/src/hdf5_package/hdf5_function.py
@@ -122,8 +122,8 @@ def get_len_hdf5(hdf5_object):
     return file_count
 
 if __name__=='__main__':
-    path = "/media/ericlab/DE59-9C00/test/result/concate"
-    concatenate_hdf5(path, os.path.join(path, "acc_real_2.hdf5"))
+    path = "/home/ericlab/Downloads/data_real_t_pipe"
+    concatenate_hdf5(path, os.path.join(path, "real_t_pipe.hdf5"))
     # input_path = "/home/ericlab/tsuchida/2022_12/annotation/Semseg/multi_object_kai/kai3228/kai.hdf5"
     # out_path = "/home/ericlab/tsuchida/2022_12/annotation/Semseg/multi_object_kai/kai3228/kai_1.hdf5"
     # change_data(input_path, out_path)
