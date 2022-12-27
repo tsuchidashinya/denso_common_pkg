cc6e999e7f75aacabcd35ec33da6b810d5209caf
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2022-12-27 14:39:08 +0900

    fix: hdf5_open_server

--- hdf5_package/scripts/hdf5_open_server.py
+++ hdf5_package/scripts/hdf5_open_server.py
@@ -37,7 +37,7 @@ class Hdf5OpenServer():
         np_concat_cloud = util_msg_data.concatenate_npcloud_and_npmask(np_cloud, mask_data)
         response = Hdf5OpenSegmentationServiceResponse()
         response.cloud_data = util_msg_data.npcloud_to_msgcloud(np_concat_cloud)
-        response.data_size = len(self.hdf5_object)
+        response.data_size = len(self.hdf5_data_dict)
         return response
     
     def real_phoxi_service_callback(self, request):
--- hdf5_package/src/hdf5_package/hdf5_function.py
+++ hdf5_package/src/hdf5_package/hdf5_function.py
@@ -168,6 +168,6 @@ def get_len_hdf5(hdf5_object):
 if __name__=='__main__':
     path = "/media/ericlab/DE59-9C00/test/concate"
     concatenate_hdf5(path, os.path.join(path, "real_HV8_data.hdf5"))
-    # input_path = "/home/ericlab/tsuchida/2022_12/annotation/Semseg/multi_object_kai/kai3228/kai.hdf5"
-    # out_path = "/home/ericlab/tsuchida/2022_12/annotation/Semseg/multi_object_kai/kai3228/kai_1.hdf5"
+    # input_path = "/home/ericlab/tsuchida/2022_12/annotation/Semseg/multi_object/kai3228/kai.hdf5"
+    # out_path = "/home/ericlab/tsuchida/2022_12/annotation/Semseg/multi_object/kai3228/kai_1.hdf5"
     # change_data(input_path, out_path)
